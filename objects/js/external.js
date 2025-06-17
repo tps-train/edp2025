9f83b3d794ada3506a3fcac1d4f18fd5e8893f1e
Steve Shilling <6329361+stevshil@users.noreply.github.com>
2025-06-17 18:06:49 +0100

    Object version now has delete and sort

--- objects/js/external.js
+++ objects/js/external.js
@@ -4,42 +4,45 @@ function generateTable() {
     let tablestring = "<table><tr><th>Name:</th><th>Score</th></tr>"
 
     for ( let counter = 0; counter < students.length; counter++ ) {
-        tablestring += `<tr><td class="student"><button id="delete" value="${counter}">${students[counter].name}</button></td><td class="score">${students[counter].score}</td>`
+        tablestring += `<tr><td class="student"><button id="delete" value="${counter}" onclick="deletestudent(${counter})">${students[counter].name}</button></td><td class="score">${students[counter].score}</td>`
     }
 
     tablestring += "</table>"
 
-    return tablestring
+    document.getElementById("scoresout").innerHTML = tablestring
 }
 
 function sortscores(a,b) {
-    return a-b
-}
-
-function sortcombinedscores(a,b) {
     return a.score - b.score
 }
 
+function deletestudent(id) {
+    let newlist = []
+    for( let counter = 0; counter < students.length; counter ++ ) {
+        if ( counter == id )
+            continue
+        newlist.push(students[counter])
+    }
+    students=newlist
+    console.log("Deleted: "+students[id].name)
+
+    generateTable()
+}
+
 // document.getElementById('add').addEventListener('click', () => {
+// Use of onclick in the html to call our function instead of an event listener
 function addScore() {
     students.push( { name: document.getElementById('student').value, score: document.getElementById("score").value } )
 
     console.log("STUDENTS: "+students.name+" & "+students.score)
 
-    let tablestring = generateTable()
-    
-    document.getElementById("scoresout").innerHTML = tablestring
+    generateTable()
 }
 // })
 
 document.getElementById('sort').addEventListener('click', () => {
-    let combined = students.map((student, idx) => ({ student, score: scores[idx] }))
-    combined.sort(sortcombinedscores)
-    console.log("SORTED:" + combined)
-    students = combined.map(item => item.student)
-    scores = combined.map(item => item.score)
+    students.sort(sortscores)
 
-    let tablestring = generateTable()
-
-    document.getElementById("scoresout").innerHTML = tablestring
+    // The assignment no longer required as the div is targetted in the function
+    generateTable()
 })
